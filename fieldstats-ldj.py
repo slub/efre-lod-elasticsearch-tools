a22fe19aefeda782d9be5e111afb12fb9453ff74
Bernhard Hering <bernhard.hering@slub-dresden.de>
2017-12-18 17:33:16 +0100

    code cleanup

--- fieldstats-ldj.py
+++ fieldstats-ldj.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3 
 # -*- coding: utf-8 -*-
-from datetime import datetime
-import collections
 import json
-from pprint import pprint
-import numpy as np
 import argparse
 import sys
-
-stats = dict()
-
-def traverse(dict_or_list, path):
-    if isinstance(dict_or_list, dict):
-        iterator = dict_or_list.items()
-    elif isinstance(dict_or_list,list):
-        iterator = enumerate(dict_or_list)
-    for k, v in iterator:
-        yield path + str([k]), v
-        if isinstance(v, (dict, list)):
-            for k, v in traverse(v, path + str([k])):
-                yield k, v
-
 
 def travpath(dol, path):
     if path and path[0]=="*":
@@ -50,14 +32,12 @@ def travpath(dol, path):
                 for i in travpath(elem[path[0]],path[1:]):
                     yield i
 
-
 def removebraces(string):
     if string[-1]==']':
         string=string[:-1]
     if string[0]=='[':
         string=string[1:]
     return string
-
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser(description='return single field statistics of an line-delimited JSON Input-Stream.\nOutput is a 2 coloumn CSV-Sheet.\nNavigate into nested fields via dots (.) wildcard operator is: *..')
@@ -69,6 +49,7 @@ if __name__ == "__main__":
 "        -help      print this help\n"\
 "        -path      which JSON Path to examine!\n")
     parr = args.path.split('.')
+    stats={}
     for line in sys.stdin:
         jline=json.loads(line)
         for v in travpath(jline,parr):
