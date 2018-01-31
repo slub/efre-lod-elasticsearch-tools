8384ddce2e4745496abfd2b384b353c54e437a4f
Bernhard Hering <bernhard.hering@slub-dresden.de>
2018-01-31 14:05:46 +0100

    update, implemented more ES-Stuff

--- update_mrc_index.py
+++ update_mrc_index.py
@@ -2,7 +2,10 @@
 import sys
 import json
 from elasticsearch import Elasticsearch
+from elasticsearch import exceptions
+from elasticsearch import helpers
 import argparse
+from es2json import eprint
 
 if __name__ == "__main__":
     #argstuff
@@ -11,14 +14,38 @@ if __name__ == "__main__":
     parser.add_argument('-port',type=int,default=9200,help='Port of the ElasticSearch-node to use, default is 9200.')
     parser.add_argument('-index',type=str,help='ElasticSearch Search Index to use')
     parser.add_argument('-type',type=str,help='ElasticSearch Search Index Type to use')
-    parser.add_argument('-prefix',type=str,help='ElasticSearch Search doc_type_prefix to use')
+    parser.add_argument('-prefix',type=str,default="",help='ElasticSearch Search doc_type_prefix to use')
     args=   parser.parse_args()
     es=Elasticsearch(host=args.host)
+    actions=[]
     
+    try:
+        es.get(index=args.index,doc_type=args.type,id="_mapping")
+    except exceptions.NotFoundError as e:
+        if 'index_not_found' in str(e):
+            eprint("Index not found!")
+        elif 'type_missing_exception' in str(e):
+            eprint("Type not correct!")
+        exit(-1)
     for line in sys.stdin:
+            update=None 
             jline=json.loads(line)
+            doc_es=None
             doc_ts=float(jline["005"][0])
-            doc_es=es.get(index=args.index,doc_type=args.type,id=str(args.prefix+jline["001"][0]),_source="005")
-            if doc_es["found"]:
+            try:
+                doc_es=es.get(index=args.index,doc_type=args.type,id=str(args.prefix+jline["001"][0]),_source="005")
+            except exceptions.NotFoundError as e:
+                update="create"
+            if doc_es:
                 if doc_ts>float(doc_es["_source"]["005"][0]):
-                    print("update")
+                    update="update"
+            if update:
+                actions.append({
+                                '_op_type':update,
+                                '_index':args.index,
+                                '_type':args.type,
+                                '_id':str(args.prefix+jline["001"][0]),
+                                'doc': jline
+                                })
+    elasticsearch.helpers.streaming_bulk(es,actions,chunk_size=500)
+            
