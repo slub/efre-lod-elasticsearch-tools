0b2f5476e006ba67b3da69f285b2febd5a6291c9
Bernhard Hering <bernhard.hering@slub-dresden.de>
2018-02-02 14:23:37 +0100

    merged es2json and getindex

--- es2json.py
+++ es2json.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-from elasticsearch import Elasticsearch
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import json
+from pprint import pprint
+import argparse
 import sys
+from es2json import esgenerator
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)   
@@ -34,3 +39,15 @@ def esgenerator(host=None,port=9200,index=None,type=None,body=None,source=True,h
                 yield hits['_source']
             else:
                 yield hits
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description='simple ES.Getter!')
+    parser.add_argument('-host',type=str,default="127.0.0.1",help='hostname or IP-Address of the ElasticSearch-node to use, default is localhost.')
+    parser.add_argument('-port',type=int,default=9200,help='Port of the ElasticSearch-node to use, default is 9200.')
+    parser.add_argument('-index',type=str,help='ElasticSearch Search Index to use')
+    parser.add_argument('-type',type=str,help='ElasticSearch Search Index Type to use')
+    parser.add_argument('-body',type=str,help='Searchbody')
+    args=parser.parse_args()
+    
+    for json_record in esgenerator(args.host,args.port,args.index,args.type,args.body,headless=True):
+        sys.stdout.write(json.dumps(json_record)+"\n")
