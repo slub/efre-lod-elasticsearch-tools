facdc466e46ad59d412f02049dd5ddd2bf36c070
Bernhard Hering <bernhard.hering@slub-dresden.de>
2018-02-26 10:47:35 +0100

    replaced install-script by Makefile for adlookup

--- adlookup/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://mdipierro.pythonanywhere.com/examples/static/web2py_src.zip
-unzip web2py_src.zip
-cp -rv controllers views modules web2py/applications/welcome
-cd web2py
-echo -e "\n\n"
-echo "Run web2py -i [INET-ADRESS_TO_LISTEN]"
