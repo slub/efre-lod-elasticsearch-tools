c26d41e422a65d507b943c6423c7ce52e8b044d3
Bernhard Hering <bernhard.hering@slub-dresden.de>
2018-02-26 14:47:15 +0100

    fixed bug in Makefile, made view and controller more dynamic

--- adlookup/controllers/default.py
+++ adlookup/controllers/default.py
@@ -5,18 +5,30 @@
 # -------------------------------------------------------------------------
 
 def index():
+    opt=None
     if request.vars.uri:
         session.uri = request.vars.uri
         redirect(URL('data'))
-    return dict()
+    else:
+        from adl import loadjson
+        config=loadjson("/etc/adlookup.json")
+        optgr=[]
+        for k,v in config["types"].items():
+            optgr.append(k)
+        opt=SELECT(optgr,_name="typ",_size="1")
+    return dict(options=opt)
 
 def data():
-    from adl import getDataByID
+    from adl import getDataByID, loadjson
     resp=[]
-    for x in getDataByID(typ=request.vars.typ,num=request.vars.uri,field=request.vars.feld):
+    for x in getDataByID(typ=request.vars.typ,num=request.vars.uri,feld=request.vars.feld):
         resp.append(x)
     if len(resp)==1:
-        return response.json(resp[0])
+        if isinstance(resp[0],str):
+            if resp[0].startswith("No config defined in"):
+                raise HTTP(503)
+        else:
+            return response.json(resp[0])
     elif len(resp)==0:
         raise HTTP(404)
     else:
