b8fb7c33d6312e0da25514003369ae02693472e7
boerni667 <bernhardhering@pakke.de>
2018-03-13 10:36:45 +0100

    id_resolver

--- enrichment/gnd2swb.py
+++ enrichment/gnd2swb.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import argparse
 import sys
 import os.path
+import requests
 import signal
 import urllib3.request
 from multiprocessing import Pool
@@ -20,7 +21,7 @@ args=None
 
 author_gnd_key="sameAs"
 
-### replace DNB IDs by SWB IDs in your ElasticSearch Index.
+### replace SWB/GND IDs by SWB IDs in your ElasticSearch Index.
 ### example usage: ./gnd2swb.py -host ELASTICSEARCH_SERVER -index swbfinc -type finc -aut_index=source-schemaorg -aut_type schemaorg
 def handle_author(author):
     try:
@@ -30,28 +31,6 @@ def handle_author(author):
     if "@id" in data["_source"]:
         return data["_source"]["@id"]
     
-def process_stuff(jline):
-    changed=False
-    if "author" in jline["_source"]:
-        if isinstance(jline["_source"]["author"],str):
-            author_id=handle_author(jline["_source"]["author"])
-            if author_id:
-                jline["_source"]["author"]=author_id
-                changed=True
-        elif isinstance(jline["_source"]["author"],dict):
-            if "@id" in jline["_source"]["author"]:
-                author_id=handle_author(jline["_source"]["author"]["@id"])
-                if author_id:
-                    jline["_source"]["author"]["@id"]=author_id
-                    changed=True
-        elif isinstance(jline["_source"]["author"],list):
-            for author in jline["_source"]["author"]:
-                if isinstance(author,dict):
-                    if author_gnd_key in author:
-                        author_id=handle_author(author["@id"])
-                        if author_id:
-                            author["@id"]=author_id
-                            changed=True
     if changed:
         es.index(index=args.index,doc_type=args.type,body=jline["_source"],id=jline["_id"])
 
@@ -90,7 +69,7 @@ def getidbygnd(gnd,cache=None):
             return
     for elastic in indices:
         http = urllib3.PoolManager()
-        url="http://194.95.145.44:9200/"+elastic["index"]+"/"+elastic["type"]+"/_search?q=sameAs:\""+uri+"\""
+        url="http://"+args.host+":9200/"+elastic["index"]+"/"+elastic["type"]+"/_search?q=sameAs:\""+uri+"\""
         try:
                 r=http.request('GET',url)
                 data = json.loads(r.data.decode('utf-8'))
@@ -108,13 +87,113 @@ def getidbygnd(gnd,cache=None):
                 else:
                     return str("http://data.slub-dresden.de/"+str(elastic["index"])+"/"+hit["_id"])
 
+def useadlookup(feld,index,uri):
+        r=requests.get("http://"+args.host+":8000/welcome/default/data?ind="+str(index)+"&feld="+str(feld)+"&uri="+str(uri))
+        if r.ok:
+            return r.json()
+        else:
+            return {}
 
+def resolve_geos(record):
+    new=None
+    for geo in ["deathPlace","birthPlace"]:
+            if geo in record:
+                if isinstance(record[geo],list):
+                    for elem in record[geo]:
+                        _id=resolve_bnode(elem,"geo")
+                        if _id and "http://data.slub-dresden.de/" in _id:
+                            record[geo]=_id
+                            return record
+                if isinstance(record[geo],dict):
+                    if _id and "http://data.slub-dresden.de/" in _id:
+                        record[geo]=_id
+                        return record
+    return None
+
+def resolve_persons(record):
+    changed=False
+    for person in ["author","relatedTo","colleague","contributor","knows","follows","parent","sibling","spouse","children"]:
+        if person in record:
+            eprint("resolve_persons()"+str(record[person]))
+            if isinstance(record[person],list):
+                for n,elem in enumerate(record[person]):
+                    resolved=resolve_bnode(elem,"persons")
+                    if resolved.startswith("http://data.slub-dresden.de/"):
+                        record[person][n]["@id"]=None
+                        record[person][n]["@id"]=litter(record[person][n]["@id"],resolved)
+                        changed=True
+            elif isinstance(record[person],dict):
+                resolved=resolve_bnode(record[person],"persons")
+                if resolved.startswith("http://data.slub-dresden.de/"):
+                    record[person]["@id"]=None
+                    record[person]["@id"]=litter(record[person]["@id"],resolved)
+                    changed=True
+            if isinstance(record[person],list): ## remove doubles by making a set out of the list and vice-versa
+                record[person]=list(set(record[person]))
+    if changed:
+        return(record)
+    else:
+        return None
+        
+def resolve_bnode(record,index):
+    if isinstance(record,dict):
+        if "@id" not in record and "sameAs" in record:
+            geo_record={}
+            if isinstance(record["sameAs"],str):
+                geo_record=useadlookup("sameAs",index,record["sameAs"])
+            elif isinstance(record["sameAs"],list):
+                for n,elem in enumerate(record["sameAs"]):
+                    if elem.startswith("http://swb.bsz"):
+                        geo_record=litter(geo_record,useadlookup("sameAs",index,elem))
+                        if "@id" in geo_record:
+                            break
+                    elif elem.startswith("http://d-nb"):
+                        geo_record=litter(geo_record,useadlookup("sameAs",index,elem))
+                        if "@id" in geo_record:
+                            break
+            if "@id" in geo_record:
+                return geo_record.get("@id")
+    return ""
     
-    
+        
+### avoid dublettes and nested lists when adding elements into lists
+def litter(lst, elm):
+    if not lst:
+        lst=elm
+    else:
+        if isinstance(lst,str):
+            lst=[lst]
+        if isinstance(elm,str):
+            if elm not in lst:
+                lst.append(elm)
+        elif isinstance(elm,list):
+            for element in elm:
+                if element not in lst:
+                    lst.append(element)
+    return lst
+
+        
 if __name__ == "__main__":
-    for elem in getDataByID(typ=None,num="http://viaf.org/viaf/5744769'"):
-        print(elem)
-
+    #argstuff
+    parser=argparse.ArgumentParser(description='Entitysplitting/Recognition of MARC-Records')
+    parser.add_argument('-host',type=str,help='hostname or IP-Address of the ElasticSearch-node to use. If None we try to read ldj from stdin.')
+    parser.add_argument('-port',type=int,default=9200,help='Port of the ElasticSearch-node to use, default is 9200.')
+    parser.add_argument('-type',type=str,help='ElasticSearch Index to use')
+    parser.add_argument('-index',type=str,help='ElasticSearch Type to use')
+    parser.add_argument('-help',action="store_true",help="print this help")
+    args=parser.parse_args()
+    if args.help:
+        parser.print_help(sys.stderr)
+        exit()
+    for hits in esgenerator(host=args.host,port=args.port,index=args.index,type=args.type,headless=True):
+            record=resolve_persons(hits)
+            if record:
+                final=resolve_geos(record)
+            else:
+                final=resolve_geos(hits)
+            if final:
+                sys.stdout.write(json.dumps(final,indent=None)+"\n")
+                sys.stdout.flush()
 
 #if __name__ == "__main__":
     #parser=argparse.ArgumentParser(description='replace DNB IDs (GND) by SWB IDs in your ElasticSearch Index!')
