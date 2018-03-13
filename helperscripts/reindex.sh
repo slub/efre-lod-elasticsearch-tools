87b4814f97bdea43fe8b318325c739ff5e58f258
boerni667 <bernhardhering@pakke.de>
2018-03-13 08:57:32 +0100

    made more dynamic

--- helperscripts/reindex.sh
+++ helperscripts/reindex.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-curl -XDELETE '$1:9200/resources'
+for i in resources orga persons geo; do
+string=${1}:9200/${i}
+curl -XDELETE ${string}
 echo ""
-curl -XPUT '$1:9200/resources' -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
+curl -XPUT ${string} -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
 echo ""
-pv CreativeWork-records.ldj | esbulk -host $1 -type schemaorg -index resources -id identifier -w 8
-curl -XDELETE '$1:9200/orga'
-echo ""
-curl -XPUT '$1:9200/orga' -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
-echo ""
-pv Organization-records.ldj | esbulk -host $1 -type schemaorg -index orga -id identifier -w 8 
-curl -XDELETE '$1:9200/persons'
-echo ""
-curl -XPUT '$1:9200/persons' -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
-echo ""
-pv Person-records.ldj | esbulk -host $1 -type schemaorg -index persons -id identifier -w 8 
-curl -XDELETE '$1:9200/geo'
-echo ""
-curl -XPUT '$1:9200/geo' -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
-echo ""
-pv Place-records.ldj | esbulk -host $1 -type schemaorg -index geo -id identifier -w 8 
+pv ${i}-records.ldj | esbulk -host $1 -type schemaorg -index ${i} -id identifier -w 8
+done
