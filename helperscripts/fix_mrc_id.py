9a980a707193ba2b0a88a9863244fdd19b2bdb49
boerni667 <bernhardhering@pakke.de>
2018-03-21 17:29:44 +0100

    put fix_mrc_id into lib to call from other tools, improvied MARC21 update workflow

--- helperscripts/fix_mrc_id.py
+++ helperscripts/fix_mrc_id.py
@@ -4,14 +4,21 @@ import json
 import sys
 from es2json import eprint
 
-for line in sys.stdin:
-    try:
-        jline=json.loads(line)
-    except:
-        eprint("corrupt json: "+str(line))
-        continue
-    _id=jline.pop("001")
-    for elem in _id:
-        jline["001"]=elem
-    sys.stdout.write(json.dumps(jline)+"\n")
+def fix_mrc_id(jline):
+    if "001" in jline and isinstance(jline["001"],list):
+        _id=jline.pop("001")
+        for elem in _id:
+            jline["001"]=elem
+    return jline
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        try:
+            jline=json.loads(line)
+        except:
+            eprint("corrupt json: "+str(line))
+            continue
+        jline=fix_mrc_id(jline)
+        sys.stdout.write(json.dumps(jline)+"\n")
+        sys.stdout.flush()
 
