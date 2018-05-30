0729d71879f29937881e55f29f05dac82e4e444e
boerni667 <bernhardhering@pakke.de>
2018-05-30 14:07:50 +0200

    bugfix regarding hostnames & stuff

--- helperscripts/reindex.sh
+++ helperscripts/reindex.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 for i in `ls -d */`; do
 index=`echo ${i} | cut -d / -f 1`
-string=${1}:9200/${index}
+#for index in works tags; do
+host=http://${1}:9200
+string=${host}/${index}
 curl -XDELETE ${string}
 echo ""
 curl -XPUT ${string} -d '{"mappings":{"schemaorg":{"date_detection":false}}}'
 echo ""
-pv ${index}/*-records.ldj | esbulk -host $1 -type schemaorg -index ${index} -id identifier -w 8
+for ldj in `ls ${index}`; do esbulk -server ${host} -type schemaorg -index ${index} -id identifier -w 8  ${index}/${ldj}; done
 done
