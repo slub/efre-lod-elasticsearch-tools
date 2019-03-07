c4fb2858285bf77045e40b19028782a62ea61b23
Bernhard Hering <bhering@sdvlodapi.slub-dresden.de>
2019-03-07 14:13:46 +0100

    for uwsgi deployment

--- api/flask_api.py
+++ api/flask_api.py
@@ -363,5 +363,5 @@ class AutSearch(Resource):
     
     
 
-        
-app.run(host="localhost",port=80,debug=True)
+if __name__ == '__main__':        
+    app.run(host="localhost",port=80,debug=True)
