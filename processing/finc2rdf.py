625b6882cd63a30ba8aad1fc5dcd7d5544609330
boerni667 <bernhard.hering@slub-dresden.de>
2019-04-02 16:23:35 +0200

    enhanced mapping and @context

--- processing/finc2rdf.py
+++ processing/finc2rdf.py
@@ -1,11 +1,47 @@
 #!/usr/bin/python3
 
+import argparse
 import sys
 import json
 
 from es2json import ArrayOrSingleValue
 
+baseuri="http://data.finc.info/resources/"
 
+
+prop2isil={"swb_id_str":"(DE-576)",
+           "kxp_id_str":"(DE-627)"
+               }
+
+def getIDs(record,prop):
+    if isinstance(prop,str):
+        if prop in prop2isil and prop in record:
+            return str(prop2isil[prop]+record[prop])
+        elif prop in record and not prop in prop2isil:
+            return str(record[prop])
+    elif isinstance(prop,list):
+        ret=[]
+        for elem in prop:
+            if elem in prop2isil and elem in record:
+                ret.append(str(prop2isil[elem]+record[elem]))
+            elif elem in record and not elem in prop2isil:
+                ret.append(record[elem])
+        if ret:
+            return ret
+
+def getoAC(record,prop):
+    if isinstance(record.get(prop),str):
+        if record.get(prop)=="Free":
+            return "Yes"
+    elif isinstance(record.get(prop),list):
+        for elem in record.get(prop):
+            if elem=="Free":
+                return "Yes"
+            
+def getAtID(record,prop):
+    if record.get(prop):
+        return baseuri+record[prop]
+        
 def getGND(record,prop):
     ret=[]
     if isinstance(record.get(prop),str):
@@ -18,6 +54,62 @@ def getGND(record,prop):
     else:
         return None
 
+def getTitle(record,prop):
+    title=getProperty(record,prop)
+    if title:
+        if isinstance(title,str):
+            if title[-2:]==" /":
+                title=title[:-2]
+        elif isinstance(title,list):
+            for n, elem in enumerate(title):
+                if elem[-2:]==" /":
+                    title[n]=title[n][:-2]
+        return title
+
+def getformat(record,prop,formattable):
+    if isinstance(record.get(prop),str) and record.get(prop) in formattable:
+            return formattable.get(record.get(prop))
+    elif isinstance(record.get(prop),list):
+        for elem in record.get(prop):
+            if elem in formattable:
+                return formattable.get(elem)
+
+def getFormatRdfType(record,prop):
+    formatmapping={ "Article, E-Article":"bibo:Article",           
+                        "Book, E-Book":"bibo:Book",
+                        "Journal, E-Journal":"bibo:Periodical",
+                        "Manuscript":"bibo:Manuscript",
+                        "Map":"bibo:Map",
+                        "Thesis":"bibo:Thesis",
+                        "Unknown Format":"bibo:Document",
+                        "Video":"bibo:AudioVisualDocument"
+                            }
+    value=getformat(record,prop,formatmapping)
+    if value:
+        return value
+                   
+
+def getFormatDctMedium(record,prop):
+    formatmapping={"Audio":"rdamt:1001",
+                         "Microform":"rdamt:1002",
+                         "Notated Music":"rdau:P60488"
+                             }
+    value=getformat(record,prop,formatmapping)
+    if value:
+        return value
+
+def getOfferedBy(record,prop):
+        if record.get(prop):
+            return {
+           "@type": "Offer",
+           "offeredBy": {
+                "@id": "https://data.finc.info/resource/organisation/DE-15",
+                "@type": "Library",
+                "name": "Univeristätsbibliothek Leipzig",
+                "branchCode": "DE-15"
+            },
+           "availability": "http://data.ub.uni-leipzig.de/item/wachtl/DE-15:ppn:"+record[prop]
+       }
 
 def getProperty(record,prop):
     ret=[]
@@ -39,18 +131,43 @@ def getProperty(record,prop):
 # mapping={ "target_field":"someString"},
 
 #           "target_field":{function:"source_field"}}
-mapping={ "dct:identifier":{getProperty:["record_id","swb_id_str","kxp_id_str","source_id"]},
+
+context={ "dct:identifier":"http://purl.org/dc/terms/dct:identifier",
+          "bibo:issn":"http://purl.org/ontology/bibo/issn",
+          "bibo:isbn":"http://purl.org/ontology/bibo/isbn",
+          "umbel:isLike":"http://umbel.org/umbel/isLike",
+          "dcterms:title":"http://purl.org/dc/terms/title",
+          "rdau:P60493":"http://rdaregistry.info/Elements/u/P60493",
+          "bibo:shortTitle":"http://purl.org/ontology/bibo/shortTitle",
+          "dcterms:alternative":"http://purl.org/dc/elements/1.1/alternative",
+          "dcterms:creator":"http://purl.org/dc/elements/1.1/creator",
+          "dcterms:contributor":"http://purl.org/dc/elements/1.1/contributor",
+          "dcterms:creator":"http://purl.org/dc/elements/1.1/creator",
+          "rdau:P60333":"http://rdaregistry.info/Elements/u/P60333",
+          "rdau:P60163":"http://rdaregistry.info/Elements/u/P60163",
+          "dc:publisher":"http://purl.org/dc/terms/publisher",
+          "dctermes:issued":"http://purl.org/dc/terms/issued",
+          "rdau:P60489":"http://rdaregistry.info/Elements/u/P60489",
+          "isbd:P1053":"http://iflastandards.info/ns/isbd/elements/P1053",
+          "dct:language":"http://purl.org/dc/terms/language",
+          "dct:isPartOf":"http://purl.org/dc/terms/isPartOf",
+          "dct:bibliographicCitation":"http://purl.org/dc/terms/bibliographicCitation",
+          "openAccessContent":"http://dbpedia.org/ontology/openAccessContent",
+          "offeredBy":"http://schema.org/offeredBy"
+          }
+
+mapping={ "@id":{getAtID:"id"},
+          "dct:identifier":{getIDs:["record_id","swb_id_str","kxp_id_str","source_id"]},
           "bibo:issn":{getProperty:"issn"},
           "bibo:isbn":{getProperty:"isbn"},
           "umbel:isLike":{getProperty:"url"},
-          "dcterms:title":{getProperty:"title"},
-          "rdau:P60493":{getProperty:["title_part","title_sub"]},
-          "bibo:shortTitle":{getProperty:"title_short"},
-          "dcterms:alternative":{getProperty:"title_alt"},
+          "dcterms:title":{getTitle:"title"},
+          "rdau:P60493":{getTitle:["title_part","title_sub"]},
+          "bibo:shortTitle":{getTitle:"title_short"},
+          "dcterms:alternative":{getTitle:"title_alt"},
           "dcterms:creator":{getProperty:"author"},
           "dcterms:contributor":{getProperty:"author2"},
           "dcterms:creator":{getGND:"author_id"},
-          #"dcterms:title":{getProperty,"title"}
           "rdau:P60333":{getProperty:"imprint"},
           "rdau:P60163":{getProperty:"publishPlace"},
           "dc:publisher":{getProperty:"publisher"},
@@ -61,8 +178,10 @@ mapping={ "dct:identifier":{getProperty:["record_id","swb_id_str","kxp_id_str","
           "dct:isPartOf":{getProperty:"hierarchy_top_id"},
           "dct:bibliographicCitation":{getProperty:["container_title","container_reference"]},
           "dct:isPartOf":{getProperty:"hierarchy_parent_id"},
-          "rdf:type":{getProperty:"format_finc"},
-          "http://dbpedia.org/ontology/openAccessContent":{getProperty:"facet_avail"}
+          "rdf:type":{getFormatRdfType:"format_de15"},
+          "dct:medium":{getFormatDctMedium:"format_de15"},
+          "openAccessContent":{getoAC:"facet_avail"},
+           "offeredBy": {getOfferedBy:"record_id"},
           }
 
 def process_field(record,source_field):
@@ -98,11 +217,27 @@ def process_line(record):
             mapline[key]=value
     mapline=removeNone(mapline)
     if mapline:
+        mapline["@context"]=context
         return mapline
     else:
         return None
     
 def main():
+    parser=argparse.ArgumentParser(description='Entitysplitting/Recognition of MARC-Records')
+    parser.add_argument('-gen_cmd',action="store_true",help='generate bash command')
+    args=parser.parse_args()
+    if args.gen_cmd:
+        fl=""
+        for k,v in mapping.items():
+            for c,w in v.items():
+                if isinstance(w,str):
+                    fl+=w+","
+                elif isinstance(w,list):
+                    for elem in w:
+                        fl+=elem+","
+        fl=fl[:-1]
+        print("solrdump -verbose -server https://index.ubl-proxy.slub-dresden.de/solr/biblio/ -q institution:DE-15 -fl {} | {}".format(fl,sys.argv[0]))
+        quit()
     for line in sys.stdin:
         target_record=process_line(json.loads(line))
         if target_record:
