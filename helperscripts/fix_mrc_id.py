7862725f46b24b10433dcfa9062599c8ab502279
boerni667 <bernhard.hering@slub-dresden.de>
2019-04-02 16:24:00 +0200

    added MarcXChange JSON validation

--- helperscripts/fix_mrc_id.py
+++ helperscripts/fix_mrc_id.py
@@ -2,7 +2,7 @@
 
 import json
 import sys
-from es2json import eprint
+from es2json import eprint, isint
 
 def fix_mrc_id(jline):
     if "001" in jline and isinstance(jline["001"],list):
@@ -13,6 +13,17 @@ def fix_mrc_id(jline):
                 return None
     return jline
 
+def valid_mrc_fields(jline):
+    if jline:
+        for key in jline:
+            if isint(key) and len(str(int(key)))>1:
+                for elem in jline[key]:
+                    if isinstance(elem,str):
+                        return None
+    return jline
+
+
+
 if __name__ == "__main__":
     for line in sys.stdin:
         try:
@@ -21,6 +32,7 @@ if __name__ == "__main__":
             eprint("corrupt json: "+str(line))
             continue
         jline=fix_mrc_id(jline)
+        jline=valid_mrc_fields(jline)
         if jline:
             sys.stdout.write(json.dumps(jline)+"\n")
             sys.stdout.flush()
