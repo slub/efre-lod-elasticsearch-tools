057101163794d47a081dbb4d9db7e8d0324bf1c7
boerni667 <bernhard.hering@slub-dresden.de>
2019-08-14 12:47:15 +0200

    fixed bug produced by the MARC21 data of the norwegian national library

--- helperscripts/marc2jsonl.py
+++ helperscripts/marc2jsonl.py
@@ -3,7 +3,7 @@
 import sys
 import json
 
-from es2json import ArrayOrSingleValue, eprint,litter
+from es2json import ArrayOrSingleValue, eprint,litter,isint
 from pymarc import MARCReader
 from six.moves import zip_longest as izip_longest
 
@@ -13,22 +13,25 @@ def transpose_to_ldj(record):
     json_record['_FORMAT'] = "MarcXchange"
     json_record['_TYPE'] = "Bibliographic"
     for field in record:
-        if field.is_control_field():
-            json_record[field.tag]=[field.data]
-        else:
-            ind="".join(field.indicators).replace(" ","_")
-            fd={}
-            for k,v in izip_longest(*[iter(field.subfields)] * 2):
-                if k=="." or k.isspace():
-                    k="_"
-                if v:
-                    fd[k]=litter(fd.get(k),v)
-            ind_obj=[]
-            for k,v in sorted(fd.items()):
-                ind_obj.append({k:v})
-            if not field.tag in json_record:
-                json_record[field.tag]=[]
-            json_record[field.tag].append({ind:ind_obj})
+        if isint(field.tag):
+            if field.is_control_field():
+                json_record[field.tag]=[field.data]
+            else:
+                ind="".join(field.indicators).replace(" ","_")
+                fd={}
+                for k,v in izip_longest(*[iter(field.subfields)] * 2):
+                    if "." in ind:
+                        ind = ind.replace(".","_")
+                    if "." in k or k.isspace():
+                        k="_"
+                    if v:
+                        fd[k]=litter(fd.get(k),v)
+                ind_obj=[]
+                for k,v in sorted(fd.items()):
+                    ind_obj.append({k:v})
+                if not field.tag in json_record:
+                    json_record[field.tag]=[]
+                json_record[field.tag].append({ind:ind_obj})
     return json_record
 
 def main():
