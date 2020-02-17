d10ac0d52c8373bd7ff3ad3b646aab31c9a68af7
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-02-17 18:06:26 +0100

    added lookups to source datahub, other indices and KXP to disambiguate bugs and problems

--- tests/entity_link_checker.py
+++ tests/entity_link_checker.py
@@ -4,10 +4,38 @@ import sys
 import json
 import argparse
 import requests
+import xml.etree.ElementTree as ET
 from es2json import esfatgenerator
 from es2json import eprint
 from es2json import isint
 from es2json import litter
+
+
+def check_other_indices(ppn):
+    r = requests.get("https://data.slub-dresden.de/swb/{ppn}".format(ppn=ppn))
+    if r.ok:
+        return True
+    return False
+
+
+def check_rawdata(index, ppn):
+    if index == "resources":
+        url = "https://data.slub-dresden.de/source/kxp-de14/{ppn}".format(ppn=ppn)
+    else:
+        url = "https://data.slub-dresden.de/source/swb-aut/{ppn}".format(ppn=ppn)
+    r = requests.get(url)
+    if r.ok:
+        return True
+    return False
+
+
+def check_swb(ppn):
+    sru_xml_data = requests.get("http://swb2.bsz-bw.de/sru/DB=2.1/username=/password=/&operation=searchRetrieve&maximumRecords=10&recordSchema=dc&query=pica.ppn:{ppn}".format(ppn=ppn))
+    if sru_xml_data.ok:
+        num_of_records = ET.fromstring(sru_xml_data.content).find("{http://www.loc.gov/zing/srw/}numberOfRecords").text
+        if int(num_of_records) > 0:
+            return True
+    return False
 
 
 def traverse(dict_or_list, path):
@@ -57,30 +85,47 @@ def run():
             _id = slashsplit[5].rsplit("?")[0]
         else:
             _id = slashsplit[5]
-    
+
     header = {"Content-type": "Application/json"}
+    sys.stdout.write("{},{},{},{},{},{}\n".format("subject",
+                                                               "path",
+                                                               "missing object",
+                                                               "wrong index?",
+                                                               "found in rawdata",
+                                                               "existent in swb"))
+    sys.stdout.flush()
     for records in esfatgenerator(host=host, port=port, index=index, type=doc_type):
         mget_body = {"docs": []}
         target_source_map = {}
         for record in records:
             for key, value in traverse(record["_source"], ""):
                 if isinstance(value, str) and value.startswith(args.base_uri):
-                    if not "source" in value:
-                        mget_body["docs"].append({"_index":value.split("/")[-2],"_id":value.split("/")[-1]})
-                        if not value in target_source_map:
-                            target_source_map[value]=[]
-                        target_source_map[value].append({key: record["_source"]["@id"]})
-        r = requests.post("http://{host}:{port}/_mget".format(host=host,port=port), json=mget_body, headers=header)
-        for doc in r.json()["docs"]:
-            if doc["found"]:
-                continue
-            else:
-                for obj in target_source_map[args.base_uri+"/"+doc["_index"]+"/"+doc["_id"]]:
-                    for key, base in obj.items():
-                        print(base, "has an 404 in", key, args.base_uri+"/"+doc["_index"]+"/"+doc["_id"])
-                #r = requests.get(value)
-                #if not r.ok:
-                    #eprint(record["@id"], "has an", r.status_code, "in",  key, value)
+                    if "source" in value:
+                        continue
+                    elif "swb" in value:
+                        continue
+                    mget_body["docs"].append({"_index":value.split("/")[-2],"_id":value.split("/")[-1]})
+                    if not value in target_source_map:
+                        target_source_map[value] = []
+                    target_source_map[value].append({key: record["_source"]["@id"]})
+        if mget_body["docs"]:
+            r = requests.post("http://{host}:{port}/_mget".format(host=host,port=port), json=mget_body, headers=header)
+            for doc in r.json().get("docs"):
+                if doc.get("found"):
+                    continue
+                else:
+                    #print(json.dumps(doc))
+                    
+                    for obj in target_source_map[args.base_uri+"/"+doc["_index"]+"/"+doc["_id"]]:
+                        for key, base in obj.items():
+                            attrib = args.base_uri+"/"+doc["_index"]+"/"+doc["_id"]
+                            sys.stdout.write("{},{},{},{},{},{}\n".format(base,
+                                                                       key,
+                                                                       attrib,
+                                                                       check_other_indices(attrib.split("/")[-1]),
+                                                                       check_rawdata(doc["_index"],attrib.split("/")[-1]),
+                                                                       check_swb(attrib.split("/")[-1])))
+                            sys.stdout.flush()
 
 
 if __name__ == "__main__":
