a405e6b08b881cc1160388802446f27016687e6f
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-02-24 18:45:30 +0100

    more fields fore KXP title data

--- helperscripts/purge_marcindex.sh
+++ helperscripts/purge_marcindex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 curl -XDELETE http://$1:9200/$2
 curl -XPUT http://$1:9200/$2/ -d '{"mappings":{"'$3'":{"date_detection":false}}}' -H 'Content-type: Application/json'
-curl -XPUT http://$1:9200/$2/_settings -d '{"index.mapping.total_fields.limit":5000}' -H 'Content-type: Application/json'
+curl -XPUT http://$1:9200/$2/_settings -d '{"index.mapping.total_fields.limit":20000}' -H 'Content-type: Application/json'
