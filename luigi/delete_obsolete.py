03eaa627cb38dc98f0cd1b84f285bda7c62f3145
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-06-15 10:25:19 +0200

    fixed timestamp string

--- luigi/delete_obsolete.py
+++ luigi/delete_obsolete.py
@@ -42,7 +42,7 @@ def swbTime2normalTime(timestring):
     ts = timestring.replace('-', '0')  # YYDDDHHMMSS or %y%j%H%M%S
     if ts[2:5] == '000':
         str_list = list(ts)
-        str_list[5] = '1'
+        str_list[4] = '1'
         ts = ''.join(str_list)
     return datetime.strptime(ts, "%y%j%H%M%S").strftime("%y%m%d")
 
