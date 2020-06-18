ff0ca07634f6a075356ead8061bc0f246f16defa
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-06-18 10:20:02 +0200

    moved stuff to other repos

--- helperscripts/fix_gnd_id.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import json
-
-for line in sys.stdin:
-    record = json.loads(line)
-    record["_id"] = record["024"][0]["7_"][0]["a"]
-    print(json.dumps(record))
--- helperscripts/reindex_fidmove.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
- curl -XDELETE ${1}/resources-fidmove-test
- curl -XPUT ${1}/resources-fidmove-test -d '{"mappings":{"schemaorg":{"date_detection":false}}}' -H 'Content-Type: Application/json'
- ~/git/efre-lod-elasticsearch-tools/processing/merge2move.py -server ${1}/resources/schemaorg | esbulk -server ${1} -index resources-fidmove-test -type schemaorg -id identifier -w 1 -verbose
- for i in  ~/git/efre-lod-elasticsearch-tools/processing/*.py; do ${i} -server ${1}/resources-fidmove-test/schemaorg | esbulk -server ${1} -index resources-fidmove-test -type schemaorg -w 1 -verbose -id identifier ; done
- curl -XDELETE ${1}/resources-fidmove
- ~/git/efre-lod-elasticsearch-tools/helperscripts/es2json.py -server ${1}/resources-fidmove-test -headless | esbulk -server ${1} -index resources-fidmove -type schemaorg -id identifier -w 1 -verbose
